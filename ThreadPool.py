14456369d5b7c576027c083fb4dc01f0a2635ac5
rmaschal <robertmaschal@gmail.com>
2018-01-15 08:48:58 +0000

    Threads die safely after tasks are complete. I'm not sure if this is the best way though (relying on TaskQueue to return None when queue is empty)

--- ThreadPool.py
+++ ThreadPool.py
@@ -1,26 +1,23 @@
 import time
 
-from threading import Thread, Condition
+from threading import Thread, Condition, Event
 from interface import implements, Interface
 from TaskQueue import *
 from Task import *
 
 class ExecutorThread(Thread):
-    def __init__(self, thread_id, condition_var, task_queue):
+    def __init__(self, thread_id, condition_var, task_queue, stop_event):
         super(ExecutorThread, self).__init__()
         self.thread_id = thread_id
-        self.is_running = False
         self.cv_ = condition_var
         self.tq_ = task_queue
+        self.stop_event = stop_event
 
     def run(self):
-        while self.is_running:
+        while not self.stop_event.is_set():
             self.cv_.acquire()
             
-            while self.tq_.is_empty():
-                if not self.is_running:
-                    return
-
+            while self.tq_.is_empty() and not self.stop_event.is_set():
                 self.cv_.wait()
             
             task = self.tq_.pop()
@@ -33,15 +30,13 @@ class ExecutorThread(Thread):
         self.is_running = True
         super(ExecutorThread, self).start()
 
-    def stop(self):
-        self.is_running = False
-
 class ThreadPool:
     def __init__(self, num_threads):
         self.num_threads = num_threads
         self.threads = []
         self.tq_ = TaskQueue()
         self.cv_ = Condition()
+        self.stop_event = Event()
 
     def submit_task(self, task):
         self.cv_.acquire()
@@ -51,7 +46,7 @@ class ThreadPool:
 
     def initialize(self):
         for i in range(self.num_threads):
-            thread = ExecutorThread(i, self.cv_, self.tq_)
+            thread = ExecutorThread(i, self.cv_, self.tq_, self.stop_event)
             self.threads.append(thread)
 
         for thread in self.threads:
@@ -62,9 +57,8 @@ class ThreadPool:
         while not self.tq_.is_empty():
             time.sleep(1)
            
-        for thread in self.threads:
-            thread.stop()
-        
+        self.stop_event.set()
+
         # wake up threads to die
         self.cv_.acquire()
         self.cv_.notifyAll()
