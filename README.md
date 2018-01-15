5c861edf9ab96486505a2f713ecf71d019496535
rmaschal <robertmaschal@gmail.com>
2018-01-15 01:32:36 +0000

    Start Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Python ThreadPool
+
+The goal of this project is to build a simple thread pool in python, capable of accepting thread run logic as a callback
---
+++ ThreadPool.py
@@ -0,0 +1,29 @@
+from threading import *
+
+class Queue:
+    def __init__(self):
+        self.q_ = []
+
+    def push(self, elt):
+        q_.push(elt)
+
+    def front(self):
+
+    def pop_front(self):
+
+class TaskQueue(Queue):
+    def __init__(self):
+
+class ExecutorThread(Thread):
+    def __init__(self):
+        self.is_running = False
+
+    def run(self):
+
+    def stop(self):
+
+class ThreadPool:
+    def __init__(self, num_threads):
+        self.num_threads = num_threads
+        self.tq_ = TaskQueue()
+         
